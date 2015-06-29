b545d1f58b1f1f9c7646da44185d5527086c1ab5
sabbir <sabbir.prog@gmail.com>
2015-06-29 13:13:09 +0600

    add all source code

---
+++ src/com/sabbir/mail/ConfigUtility.java
@@ -0,0 +1,54 @@
+package com.sabbir.mail;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+/**
+ * A utility class that reads/saves SMTP settings from/to a properties file.
+ * @author www.codejava.net
+ *
+ */
+public class ConfigUtility {
+	private File configFile = new File("smtp.properties"); 
+	private Properties configProps;
+	
+	public Properties loadProperties() throws IOException {
+		Properties defaultProps = new Properties();
+		// sets default properties
+		defaultProps.setProperty("mail.smtp.host", "smtp.gmail.com");
+		defaultProps.setProperty("mail.smtp.port", "587");
+		defaultProps.setProperty("mail.user", "sabbir@gmail.com");
+		defaultProps.setProperty("mail.password", "secret");
+		defaultProps.setProperty("mail.smtp.starttls.enable", "true");
+		defaultProps.setProperty("mail.smtp.auth", "true");
+		
+		configProps = new Properties(defaultProps);
+		
+		// loads properties from file
+		if (configFile.exists()) {
+			InputStream inputStream = new FileInputStream(configFile);
+			configProps.load(inputStream);
+			inputStream.close();
+		}
+		
+		return configProps;
+	}
+	
+	public void saveProperties(String host, String port, String user, String pass) throws IOException {
+		configProps.setProperty("mail.smtp.host", host);
+		configProps.setProperty("mail.smtp.port", port);
+		configProps.setProperty("mail.user", user);
+		configProps.setProperty("mail.password", pass);
+		configProps.setProperty("mail.smtp.starttls.enable", "true");
+		configProps.setProperty("mail.smtp.auth", "true");
+		
+		OutputStream outputStream = new FileOutputStream(configFile);
+		configProps.store(outputStream, "host setttings");
+		outputStream.close();
+	}	
+}
